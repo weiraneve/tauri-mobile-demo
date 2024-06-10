0f3cba049fc41325188ef69e739c8f75df76efd2
weiraneve <907221539@qq.com>
2024-06-10 11:34:13 +0800

    refactor: format code

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,10 +1,10 @@
 #![cfg_attr(
-all(not(debug_assertions), target_os = "windows"),
-windows_subsystem = "windows"
+    all(not(debug_assertions), target_os = "windows"),
+    windows_subsystem = "windows"
 )]
 
 use app::AppBuilder;
 
 pub fn main() {
-  AppBuilder::new().run();
+    AppBuilder::new().run();
 }
